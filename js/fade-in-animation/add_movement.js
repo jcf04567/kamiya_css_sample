8f4cc103eeca0d1dd1f88a0dcb95c7a5b3776c5c
kamiya <78120565+jcf04567@users.noreply.github.com>
2022-11-13 09:08:05 +0900

    section6 appearボタンを目的に合わせてテキストを変える

--- js/fade-in-animation/add_movement.js
+++ js/fade-in-animation/add_movement.js
@@ -1,5 +1,12 @@
 function add_movement() {
   document.querySelector('.appear').classList.toggle('inview');
+
+  const add_movement_btn = document.querySelector('#add_movement_btn');
+  if (add_movement_btn.innerText === 'appear') {
+    add_movement_btn.innerText = 'clear';
+  } else {
+    add_movement_btn.innerText = 'appear';
+  }
 }
 
 export default add_movement;
